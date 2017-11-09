c573ca2b68c00be9bf16960ee47fef3edc29f0ca
GEDC_SSavosyuk <stanislav.savosyuk.ge.com>
2017-11-09 10:37:10 +0200

    Added showcase of bean scopes

--- scopes/src/main/java/com/ssn/spring/scopes/TestBean.java
+++ scopes/src/main/java/com/ssn/spring/scopes/TestBean.java
@@ -1,10 +1,5 @@
 package com.ssn.spring.scopes;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
-
 public class TestBean {
 
     public Object obj = new Object();
